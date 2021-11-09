b10f95b50104f6ffcbc7229ae0d9b407df15fac0
Faridulakthar <faridulakthar@gmail.com>
2021-11-09 13:27:37 +0530

    ADD: redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,90 +1,85 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+// import Fade from 'react-reveal/Fade';
 
-function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
-    return (
-        <Wrap bgImage={backgroundImg} >
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
-            <Buttons>
-            <ButtonGroup>
-                <LeftButton>
-                    {leftBtnText}
-                </LeftButton>
-                {rightBtnText && 
-                 <RightButton>
-                    {rightBtnText}
-                 </RightButton>
-                }
-            </ButtonGroup>
-            <DownArrow src="/images/down-arrow.svg" />
-            </Buttons>
-        </Wrap>
-    )
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
 }
 
-export default Section
-
+export default Section;
 
 const Wrap = styled.div`
-   height: 100vh;
-   width: 100vw;
-   background-size: cover;
-   background-position: center;
-   background-repeat: no-repeat;
-   /* background-image: url('./images/model-s.jpg'); */
-   display: flex;
-   flex-direction: column;
-   justify-content: space-between; //Vertical Aligment
-   align-items: center; //horizontal alignment
-   background-image: ${props => `url("/images/${props.bgImage}")`};
-`
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  /* background-image: url('./images/model-s.jpg'); */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //Vertical Aligment
+  align-items: center; //horizontal alignment
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
 
 const ItemText = styled.div`
-    padding-top: 15vh;
-    text-align: center;
-
-`
+  padding-top: 15vh;
+  text-align: center;
+`;
 
 const ButtonGroup = styled.div`
-    display: flex;
-    margin-bottom: 30px;
-    @media (max-width: 768px) {
-        flex-direction: column;
-    }
-
-`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 const LeftButton = styled.div`
-    background-color: rgba(23, 26, 32, 0.8);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100px;
-    text-transform: uppercase;
-    opacity: 0.85;
-    font-size: 12px;
-    cursor: pointer;
-    margin: 8px;
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  text-transform: uppercase;
+  opacity: 0.85;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 
 const RightButton = styled(LeftButton)`
-    background: white;
-    opacity: 0.65;
-    color: black;
-`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
 
 const DownArrow = styled.img`
-    height: 40px;
-    overflow-x: hidden;
-    animation: animateDown infinite 1.5s;
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
 
-`
-
-const Buttons = styled.div`
-
-`
+const Buttons = styled.div``;
